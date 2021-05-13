cba8fddb2e9065d9388801ecfb3edc8d5e31306f
pvijaykrishna549 <you@example.com>
2021-05-13 15:50:15 +0530

    UC-4: Phone Number Validation Including Country Code

--- main/java/com/uservalidation/UserValidatior.java
+++ main/java/com/uservalidation/UserValidatior.java
@@ -7,22 +7,54 @@ public class UserValidatior {
 
     private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
 
-    public boolean validateFirstName(String fname)
-    {
+    public boolean validateFirstName(String fname) {
         Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
         return pattern.matcher(fname).matches();
     }
+
     private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
-    public boolean validateLastName(String lname)
-    {
+
+    public boolean validateLastName(String lname) {
         Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
         return pattern.matcher(lname).matches();
     }
+
     private static final String EMAIL_PATTERN = "^[a-z]{3}[.][xyz]*[@][a-z]{2}[.][co]{2}[.][in]*$";
-    public boolean validateEmail(String email)
-    {
+
+    public boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         return pattern.matcher(email).matches();
     }
 
-}
+    private static final String PHONENUMBER_PATTERN = "^[1-9]{2}\s[1-9][0-9]{9}$";
+    public boolean validatePhone(String phone)
+    {
+        Pattern pattern = Pattern.compile(PHONENUMBER_PATTERN);
+        return  pattern.matcher(phone).matches();
+    }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
