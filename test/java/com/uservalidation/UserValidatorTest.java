20557c646db3fe6de76bfb2a6b8e0fc309ed9f1a
pvijaykrishna549 <you@example.com>
2021-05-13 14:56:45 +0530

    UC-2: LastName Validator Test

--- test/java/com/uservalidation/UserValidatorTest.java
+++ test/java/com/uservalidation/UserValidatorTest.java
@@ -7,8 +7,15 @@ public class UserValidatorTest {
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserValidatior validatior = new UserValidatior();
-        boolean result = validatior.validateFirstName("Narayan");
+        boolean result = validatior.validateFirstName("Vijay");
        // Assertions.assertEquals(true, result);
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue()
+    {
+        UserValidatior validatior = new UserValidatior();
+        boolean result = validatior.validateLastName("Krishna");
         Assertions.assertTrue(result);
     }
 }
